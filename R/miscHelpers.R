47811f81acc04854826cf7df1048408a916ef446
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-10-28 12:45:30 -0700

    Added transpose option to extract transform

--- R/miscHelpers.R
+++ R/miscHelpers.R
@@ -7,6 +7,7 @@
 ##' @param transpose Logical (Default: FALSE). Only applicable if two dimensional.
 ##' Intended for use when an array of vectors is used.
 ##' When TRUE, will transpose such that each row is a vector element, and each column is a vector.
+##' @param stan Logical (Default: FALSE). If true, will not repermute; will be in same order as stan provides.
 ##' @param ... Not used.
 ##' @return If par is a vector, then a PxS array.
 ##' If par is matrix, then a RxCxS array.
@@ -14,7 +15,7 @@
 ##' If par is array of vectors, then a AxRxS array (unless transpose = TRUE).
 ##' @author Stephen R. Martin
 ##' @keywords internal
-.extract_transform <- function(object, par, nsamples = NULL, transpose = FALSE, ...) {
+.extract_transform <- function(object, par, nsamples = NULL, transpose = FALSE, stan = FALSE, ...) {
     samps <- as.matrix(object, pars = par)
     if (!is.null(nsamples)) {
         samps <- samps[1:nsamples, , drop = FALSE]
@@ -32,10 +33,10 @@
     lengthDims <- length(maxDims)
 
     structured_samps <- array(t(samps), dim=c(maxDims, nsamples))
-    if (lengthDims == 2 & transpose) {
+    if (lengthDims == 2 & transpose & !stan) {
         structured_samps <- aperm(structured_samps, c(2, 1, 3))
     }
-    if (lengthDims > 2) {
+    if (lengthDims > 2 & !stan) {
         structured_samps <- aperm(structured_samps, c((lengthDims - 1):(lengthDims), 1:(lengthDims - 2), lengthDims + 1))
     }
     return(structured_samps)
