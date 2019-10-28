f9f593d561fea5e4556ea16558247fc5bde4068a
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-10-28 14:40:55 -0700

    Added rstantools.R and nsamples.omegad

---
+++ R/rstantools.R
@@ -0,0 +1,12 @@
+##' Extracts the number of posterior samples tored in a fitted omegad model.
+##'
+##' @title Extract number of posterior samples.
+##' @param object omegad object.
+##' @return Integer. How many posterior post-warmup samples were drawn.
+##' @author Stephen R. Martin
+##' @importFrom rstantools nsamples
+##' @keywords internal
+nsamples.omegad <- function(object){
+   n.samps <- (object$fit@sim$iter - object$fit@sim$warmup)*object$fit@sim$chains
+   return(n.samps)
+}
