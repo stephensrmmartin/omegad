57aed5e2f5da6f1e57cefeea1e3ae6ccb59752bf
Stephen R. Martin <stephenSRMMartin@gmail.com>
2019-10-28 14:42:41 -0700

    Predict progress.

--- R/predict.R
+++ R/predict.R
@@ -6,18 +6,61 @@
 ##' @param newdata Data.frame. Must contain the exogenous (if used) and latent factor names.
 ##' @param summary Logical (Default: TRUE). Whether to return summary or array of samples.
 ##' @param prob Numeric (Default: .95). 
-##' @param samples Numeric (Default: All). Number of posterior samples to use.
+##' @param nsamples Numeric (Default: All). Number of posterior samples to use.
 ##' @param error Logical (Default: TRUE). Whether the predicted latent variables are sampled with stochastic error or not.
 ##' @param ... Unused.
+##' @param samples Numeric (Default: All). Number of posterior samples to use.
 ##' @return Data.frame (summary=TRUE) or array (summary=FALSE).
 ##' @author Stephen R. Martin
 ##' @export
-predict.omegad <- function(object, newdata, summary, prob = .95, samples, error = TRUE, ...) {
-    
+predict.omegad <- function(object, newdata, summary, prob = .95, nsamples = NULL, error = TRUE, ...) {
+    if (is.null(nsamples)) {
+        nsamples <- nsamples(omegad)
+    }
+
+
+    d <- .parse_formula.predict(object, newdata)
+    if (object$meta$gp) {
+        predOut <- .predict_gp_posterior(object, d, samples, error)
+    }
 }
 
+##' Converts object and newdata into appropriate design matrices.
+##'
+##' @title Create data structures needed for prediction.
+##' @inheritParams predict.omegad
+##' @return List of data structures for prediction.
+##' @author Stephen R. Martin
+##' @keywords internal
 .parse_formula.predict <- function(object, newdata) {
-    
+    if (any(is.na(newdata))) {
+        stop("Predict requires complete data.")
+    }
+
+    # Exogenous MM #
+    exoForm <- object$meta$modelForms$exo
+    mm.exo <- model.matrix(exoForm, newdata)
+
+    # Latent MM #
+    latNames <- unlist(object$meta$fnames$factor)
+    if (any(!(latNames %in% colnames(newdata)))) {
+        stop("Latent factor scores must be included, and named.")
+    }
+    mm.lat <- as.matrix(newdata[,latNames, drop = FALSE])
+
+    # Misc #
+    N <- nrow(mm.lat)
+    M <- object$meta$M
+    F <- object$meta$F
+    P <- ncol(mm.exo)
+
+    out <- list(N = N,
+                F = F,
+                P = P,
+                M = M,
+                theta_loc = mm.lat,
+                exo_x = mm.exo)
+    return(out)
 }
 
 ##############
@@ -29,19 +72,35 @@ predict.omegad <- function(object, newdata, summary, prob = .95, samples, error 
 # f(x) = L(K)*gp_z, gp_z ~ N(0,1) ## Standard GP
 # f(x) = phi*(D[spds]*gp_z), gp_z ~ N(0,1) ## GPA
 
-.predict_gp_posterior <- function(){
-    F <- ncol(theta_loc)
+.predict_gp_posterior <- function(object, data, nsamples, error){
+    theta_loc <- data$theta_loc
+    exo_x <- data$exo_x
+    N <- data$N
+    F <- data$F
+    P <- object$meta$P
+    M <- data$M
+    exo <- object$meta$exo
     L <- 3*5/2
     lambdas <- .lambdas(L, M)
+    F_inds <- object$stan_data$F_inds
 
-    x_phis <- lapply(1:F, function(f) {
+    # Create Basis Functions
+    gp_theta_phis <- lapply(seq_len(F), function(f) {
         .basis_phis(L, M, theta_loc[,f])
     })
-    x_exo_phis <- lapply(1:P, function(p) {
-        .basis_phis(L, M, exo_x[,p])
-    })
+    if (exo) {
+        gp_exo_phis <- lapply(seq_len(P - 1), function(p) {
+            .basis_phis(L, M, exo_x[, p + 1])
+        })
+    }
 
-    
+    # Extract posterior samples of needed params.
+    gp_z <- .extract_transform(object$fit, "gp_z", nsamples = nsamples)
+    gp_alpha <- .extract_transform(object$fit, "gp_alpha", nsamples = nsamples)
+    gp_rho <- .extract_transform(object$fit, "gp_rho", nsamples = nsamples)
+    lambda_loc_mat <- .extract_transform(object$fit, "lambda_loc_mat", nsamples = nsamples)
+    lambda_sca_mat <- .extract_transform(object$fit, "lambda_sca_mat", nsamples = nsamples)
+    nu_sca <- .extract_transform(object$fit, "nu_sca", nsamples = nsamples)
 }
 
 .predict_gp <- function(x, gp_linear_beta, gp_alpha, gp_rho, gp_z, M) {
