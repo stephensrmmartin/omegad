7e26a39d04ec33b3a9a00d7c3a72f534ab2b24b7
Stephen Martin <stephensrmmartin@gmail.com>
2019-11-01 17:46:24 -0700

    Doc update.

---
+++ R/fitted.R
@@ -0,0 +1,10 @@
+##' Extracts estimated Error factor scores and reliability estimates for the fitted data.
+##'
+##' @title Extract fitted error factor scores, omega1, and omega2 values.
+##' @inheritParams predict.omegad
+##' @inherit predict.omegad return 
+##' @author Stephen R. Martin
+##' @export
+fitted.omegad <- function(object, summary = TRUE, prob = .95, ...) {
+    
+}
