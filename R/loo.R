89b11d573933c2c5d80d421aaa3a7bf554b4094b
Stephen Martin <stephensrmmartin@gmail.com>
2019-11-07 11:28:34 -0800

    loo.omegad skeleton.

---
+++ R/loo.R
@@ -0,0 +1,14 @@
+##' LOO method for omegad objects.
+##'
+##' LOO method for omegad objects.
+##' @title omegad method for computing leave-one-out CV scores.
+##' @param object omegad object.
+##' @param ... 
+##' @return loo object.
+##' @author Stephen R. Martin
+##' @import loo
+##' @export loo
+##' @export
+loo.omegad <- function(object, ...) {
+    
+}
